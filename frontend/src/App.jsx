325cf9d5cd03b3d6798eed55200ec92dd0ffb189
Edberaga <edbetjonnathan@gmail.com>
2024-06-24 05:08:34 +0700

    Fully develop the frontend app

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,35 +1,34 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useEffect, useState } from 'react'
+import { Link, Route, Routes } from 'react-router-dom';
+import './App.scss'
+import axios from 'axios';
+import Navbar from './components/Navbar';
+import Home from './pages/Home';
+import Register from './components/Register';
+import Login from './components/Login';
+import Task from './pages/Tasks';
+import { useDispatch } from 'react-redux';
+import { authActions } from './data';
 
 function App() {
-  const [count, setCount] = useState(0)
+  const dispatch = useDispatch();
+  useEffect(() => {
+    const id = sessionStorage.getItem("id");
+    if(id) {
+      dispatch(authActions.login());
+    }
+  }, [])
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
-}
+  <main>
+    <Navbar/>
+    <Routes>
+      <Route path='/' Component={Home} />
+      <Route path='/task' Component={Task}/>
+      <Route path='/register' Component={Register} />
+      <Route path='/login' Component={Login} />
+    </Routes>
+  </main>
+)}
 
 export default App
--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,10 +1,19 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
-import './index.css'
+import 'react-toastify/dist/ReactToastify.css';
+import { ToastContainer } from 'react-toastify'
+import { BrowserRouter } from 'react-router-dom'
+import { Provider } from 'react-redux';
+import { store } from './data/index.js';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <Provider store={store}>
+        <App />
+        <ToastContainer />
+      </Provider>
+    </BrowserRouter>
   </React.StrictMode>,
 )
